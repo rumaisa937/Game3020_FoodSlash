a17feff7acc4f7a597281d9e971f67598532e361
rumaisa937 <rumaisa.shoukat12@gmail.com>
2025-09-25 00:53:03 -0400

    Game build created added slicer and countdown timer and score text at the top

---
+++ Game3020_MyProject/Assets/Assets/Scripts/Blade.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blade : MonoBehaviour {
+
+	int vertexCount = 0;
+	bool mouseDown = false;
+	LineRenderer line;
+
+
+	void Awake () {
+		line = GetComponent<LineRenderer> ();
+	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		// Android specific code
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.touchCount > 0) {
+				// Gets only the first touch (No multi-touch)
+				if (Input.GetTouch (0).phase == TouchPhase.Moved) {
+					line.positionCount = vertexCount + 1;
+					Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+					line.SetPosition (vertexCount, mousePos);
+					vertexCount++;
+
+					BoxCollider2D box = gameObject.AddComponent<BoxCollider2D> ();
+					box.transform.position = line.transform.position;
+					box.size = new Vector2 (0.1f, 0.1f);
+				}
+
+				if (Input.GetTouch (0).phase == TouchPhase.Ended) {
+					line.positionCount = 0;
+					vertexCount = 0;
+
+					BoxCollider2D[] colliders = GetComponents<BoxCollider2D> ();
+					foreach (BoxCollider2D box in colliders) {
+						Destroy (box);
+					}
+				}
+			}
+
+		// Windows specific code (Editor)
+		} else if (Application.platform == RuntimePlatform.WindowsEditor) {
+
+			if (Input.GetMouseButtonDown (0)) {
+				mouseDown = true;
+			}
+
+			if (mouseDown) {
+				line.positionCount = vertexCount + 1;
+				Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				line.SetPosition (vertexCount, mousePos);
+				vertexCount++;
+
+				BoxCollider2D box = gameObject.AddComponent<BoxCollider2D> ();
+				box.transform.position = line.transform.position;
+				box.size = new Vector2 (0.1f, 0.1f);
+			}
+
+			if (Input.GetMouseButtonUp (0)) {
+				mouseDown = false;
+				line.positionCount = 0;
+				vertexCount = 0;
+
+				BoxCollider2D[] colliders = GetComponents<BoxCollider2D> ();
+				foreach (BoxCollider2D box in colliders) {
+					Destroy (box);
+				}
+			}
+		}
+	} // Update
+
+	
+} // LineCreator
