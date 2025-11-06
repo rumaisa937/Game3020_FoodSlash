02c81e1cda8d291a404d0a6c0b9f9630783c249a
rumaisa937 <rumaisa.shoukat12@gmail.com>
2025-11-05 20:12:09 -0500

    Main Menu added

--- Game3020_MyProject/Assets/Assets/Scripts/MainMenuController.cs
+++ Game3020_MyProject/Assets/Assets/Scripts/MainMenuController.cs
@@ -5,8 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
 
-	public void PlayGame () {
-		SceneManager.LoadScene ("Level1");
+	public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+	
+	public void QuitGame()
+	{
+		Debug.Log("Quit!");
+		Application.Quit();
 	}
 
 } // MainMenuController
